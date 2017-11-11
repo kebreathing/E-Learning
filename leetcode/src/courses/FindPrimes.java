fa164943d0f0a116069014839edb0e8d4a8e3499
Chris <463720769@qq.com>
2017-11-11 14:01:52 +0800

    应用密码学

---
+++ leetcode/src/courses/FindPrimes.java
@@ -0,0 +1,34 @@
+package courses;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindPrimes {
+
+    public void findPrimes(int num){
+        Set<Integer> primes = new HashSet<>();
+        for(int i = 1; i < num; i++){
+            boolean isprime = true;
+            for(int j = 2; j < i; j++){
+                if(i % j == 0) {
+                    isprime = false;
+                    break;
+                }
+            }
+            if(isprime) primes.add(i);
+        }
+
+        // 判断是否存在两素数的乘积
+        for(int p : primes){
+            if(num % p == 0 && primes.contains(num / p)){
+                System.out.println("(" + p + "," + (num/p) + ")");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        FindPrimes fp = new FindPrimes();
+        fp.findPrimes(3599);
+        fp.findPrimes(35);
+    }
+}
---
+++ leetcode/src/courses/Mod.java
@@ -0,0 +1,13 @@
+package courses;
+
+public class Mod {
+
+    public static int encrypt(double m, double e, int n){
+        return (int)(Math.pow(m,e)) % n;
+    }
+
+    public static void main(String[] args){
+        System.out.println(encrypt(5,7,33));
+        System.out.println(encrypt(9,3,55));
+    }
+}
